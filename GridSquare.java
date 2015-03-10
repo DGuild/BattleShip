145be5f0c77ae505106a92a25cdf02f194839cb7
Matthew Beal <ohno21212@gmail.com>
2015-03-10 17:25:49 -0400

    Added methods to ShipSection class

--- GridSquare.java
+++ GridSquare.java
@@ -1,5 +1,17 @@
 public class GridSquare{
 
    ShipSection shipSection;
+   
+   public void getShipSection(){
+      return shipSection;
+   }
+   
+   public void setShipSection(ShipSection s){
+    this.shipSection = s;
+   }
+   
+   public boolean isOccupied(){
+      return (shipSection != null;
+   }
 
 }
