39bc0cecda0b3847751beb2a0ac50aa012cd0014
Drew Guild <drew.guild@gmail.com>
2015-03-11 14:18:10 -0400

    Added methods to Ship class

--- Ship.java
+++ Ship.java
@@ -1,3 +1,25 @@
 public class Ship{
 
+   private ShipSection[] sections;
+   
+   /**
+   * Creates a ship with n sections
+   */
+   public Ship(int n){
+      sections = new ShipSection[n];
+   }
+   
+   /**
+   * Returns true if all ship sections have been hit. Otherwise, returns false
+   */
+   public boolean isSunk(){
+      for(section : sections){
+         if (!section.isHit){
+            return false
+         }
+      }
+      return true;
+   }
+   
+   
 }
