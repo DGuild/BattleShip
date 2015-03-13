5ec633fa3c3edfe515319d5bbe1453f0657a236c
Drew Guild <drew.guild@gmail.com>
2015-03-13 09:14:24 -0400

    Started EastComputerAI

--- ComputerAI.java
+++ ComputerAI.java
@@ -1,3 +1,9 @@
+import java.util.*;
 public interface ComputerAI{
 
+   Stack nextGuesses = new Stack();
+   
+   public void addMove();
+   
+   public GridSquare generateRandomGridSquare();
 }
--- EasyComputerAI.java
+++ EasyComputerAI.java
@@ -1,3 +1,19 @@
+import java.util.*;
 public class EasyComputerAI implements ComputerAI{
 
+   private Stack nextGuesses = new Stack();
+   
+   public void addMove(){
+      GridSquare guess = this.generateRandomGridSquare();
+      nextGuesses.push(guess);
+   }
+   
+   public GridSquare generateRandomGridSquare(Grid g){
+      Random r = new Random();
+      int row = r.randInt(10);
+      int col = r.randInt(10);
+      GridSquare square = g.getGridSquare(row,col); 
+      return square;
+   }
+   
 }
