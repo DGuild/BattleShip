d5613ebaa647c9e5203994a532f2e2553460b2e8
Drew Guild <drew.guild@gmail.com>
2015-04-10 16:58:22 -0400

    Debugged the shit out of Medium AI

--- MediumComputerAI.java
+++ MediumComputerAI.java
@@ -1,3 +1,6 @@
+import java.util.Stack;
+import java.util.ArrayList;
+import java.util.Random;
 public class MediumComputerAI implements ComputerAI{
 
    private Stack nextGuesses = new Stack();
@@ -8,7 +11,7 @@ public class MediumComputerAI implements ComputerAI{
    
    public void addMove(){
       if(targetMode){
-         GridSquare[] guesses = this.generateRandomGridSquare(this.grid, lastGuess);
+         GridSquare[] guesses = this.generateTargetedGridSquare(this.grid, lastGuess);
          for(GridSquare guess : guesses){
             if(!alreadyGuessedSquares.contains(guess)){
                nextGuesses.push(guess);
@@ -18,7 +21,7 @@ public class MediumComputerAI implements ComputerAI{
       else{
          GridSquare guess = this.generateRandomGridSquare(this.grid);
          while(alreadyGuessedSquares.contains(guess)){ //Get new squaure, if one is already guessed
-            guess = this.generateRandomGridSquare
+            guess = this.generateRandomGridSquare(grid);
          }
          nextGuesses.push(guess);
       }
@@ -26,20 +29,21 @@ public class MediumComputerAI implements ComputerAI{
    }
    
    public GridSquare getNextMove(){
-      GridSquare guess = nextGuesses.pop();
+      GridSquare guess = (GridSquare) nextGuesses.pop();
+      alreadyGuessedSquares.add(guess);
       return guess;
       
    }
    
    public void toggleTargetMode(){
-      if(this.lastGuess.hasShipSection()) targetMode = true;
+      if(this.lastGuess.isOccupied()) targetMode = true;
       else targetMode = false;
    }
    
-   private GridSquare generateRandomGridSquare(Grid g){
+   public GridSquare generateRandomGridSquare(Grid g){
       Random r = new Random();
-      int col = r.randInt(10);
-      int row = r.randInt(10);
+      int col = r.nextInt(10);
+      int row = r.nextInt(10);
       GridSquare square = g.getGridSquare(col, row); 
       return square;
    }
@@ -47,7 +51,7 @@ public class MediumComputerAI implements ComputerAI{
    /**
    * Generates an array of Grid Squares that surround a given Grid Square
    */
-   private GridSquare[] generateTargetedGridSquare(Grid g, GridSquare s)
+   private GridSquare[] generateTargetedGridSquare(Grid g, GridSquare s){
       GridSquare[] squares = new GridSquare[4];
       int targetCol = s.getRow();
       int targetRow = s.getCol();
@@ -56,5 +60,6 @@ public class MediumComputerAI implements ComputerAI{
       squares[2] = g.getGridSquare(targetCol, targetRow + 1); //square to the bottom
       squares[3] = g.getGridSquare(targetCol - 1, targetRow); //square to the left
       return squares;
+   }
 
 }
