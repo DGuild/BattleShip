d74c7ec61bfcca7be8750e9efb937a0e85cd5461
Matthew Beal <ohno21212@gmail.com>
2015-04-19 12:50:11 -0400

    button and some changes

--- MainGui.java
+++ MainGui.java
@@ -59,7 +59,7 @@ public class MainGui extends JFrame{
         ImageIcon image2 = new ImageIcon("square.png");
         ImageIcon image3 = resizeImage(image2);
         for(int i=0; i<100; i++){
-         JLabel tile = new JLabel(image3);
+         JButton tile = new JButton(image3);
          grid.add(tile);
         }
          //set up shipyard
@@ -176,18 +176,6 @@ public class MainGui extends JFrame{
 
    }
       public ImageIcon resizeImage(ImageIcon i){
-      /*
-         Image img = i.getImage();
-         //Now create a buffered image the same size as the image:
-         BufferedImage bi = new BufferedImage(img.getWidth(null), img.getHeight(null), BufferedImage.TYPE_INT_ARGB);
-         //Then the icon image to the buffered image, and resize it as you do so:
-         Graphics g = bi.createGraphics();
-         g.drawImage(img, 0, 0, 33, 33, null);
-         //(The code above may be incorrect - check the docs)
-         //Now recreate the IconImage with the new buffered image:
-         ImageIcon newIcon = new ImageIcon(bi);
-         return newIcon;
-         */
          Image image = i.getImage(); // transform it 
          Image newimg = image.getScaledInstance(33, 33,  java.awt.Image.SCALE_SMOOTH); // scale it the smooth way  
          ImageIcon b= new ImageIcon(newimg);
