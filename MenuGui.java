c10bba37023e602c6558132b163f695ee608c20d
awbrunet <awbrunet@uvm.edu>
2015-04-19 11:59:51 -0400

    GUI update
    ugly but does things

--- MenuGui.java
+++ MenuGui.java
@@ -7,28 +7,75 @@ import javax.*;
 import javax.imageio.*;
 import java.awt.image.BufferedImage;
 
-public class MenuGui	extends JFrame{
+public class MenuGui	extends JFrame implements ActionListener{
 	private static	final	int WIDTH =	1000;
 	private static	final	int HEIGHT = 1000;
 	
-   private JLabel title, background;
+   private JLabel title, background, helpText;
+   private JFrame main, helpScreen;
+   private JPanel screen,buttons;
+   private JButton play,help;
+   private JFrame about;
    
 	public MenuGui(){
-   title = new JLabel("Begin Battleship!", SwingConstants.CENTER);
    
-   setLayout(new BorderLayout());
-   background=new JLabel(new ImageIcon("battleship_background.png"));//LOOKIT IT'S PRETTY
-   add(background);
-   background.setLayout(new FlowLayout());
+  //  JPanel screen = new JPanel();
+//    LayoutManager overlay = new OverlayLayout(screen);
+//    screen.setLayout(overlay);
+//    
+//    JPanel bg = new JPanel(new FlowLayout());
+//    background = new JLabel(new ImageIcon("battleship_background.png"));//LOOKIT IT'S PRETTY
+//    
+//    bg.add(background, BorderLayout.CENTER);
+//    screen.add(bg);
+//    add(screen, BorderLayout.CENTER);
+//    
+//    buttons = new JPanel();   
+//    play = new JButton("Play Game!");
+//    help = new JButton("How to Play");
+//    
+//    
+//    buttons.add(play);
+//    
+//    buttons.add(help);
+//    
+//    
+//    screen.add(buttons);
+
+   JPanel screen = new JPanel(new FlowLayout());
+   buttons = new JPanel();
+   buttons.setLayout(new BoxLayout(buttons, BoxLayout.PAGE_AXIS));
+   play = new JButton("Play Game!");
+   play.addActionListener(this);
+   help = new JButton("How to Play");
+   help.addActionListener(this);
    
+   buttons.add(play);
+   buttons.add(help);
    
-   //Container pane = getContentPane();
-   background.add(title);
-   
+   screen.add(buttons, BorderLayout.CENTER);
+   add(screen);
+  
+      
    setSize(WIDTH, HEIGHT);
    setVisible(true);
    setDefaultCloseOperation(EXIT_ON_CLOSE);
+   
+   helpScreen = new JFrame();
+   helpScreen.setSize(WIDTH, HEIGHT);
+   helpText = new JLabel("test");
+   helpScreen.add(helpText);
 	}
+   public void actionPerformed(ActionEvent e){
+      if(e.getSource() == play){
+         //MainGui game = new MainGui();
+         //game.setVisible(true);
+      }
+      if(e.getSource() == help){
+         
+         helpScreen.setVisible(true);
+      }
+   }
    
    public static void main(String[] args){
       MenuGui menu = new MenuGui();
