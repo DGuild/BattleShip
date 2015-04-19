6c93945dc1805f721cbaa97c528a8921d0cfd28e
awbrunet <awbrunet@uvm.edu>
2015-04-19 13:21:04 -0400

    GUI prittier
    fixes

--- MenuGui.java
+++ MenuGui.java
@@ -13,64 +13,38 @@ public class MenuGui	extends JFrame implements ActionListener{
 	
    private JLabel title, background, helpText;
    private JFrame main, helpScreen;
-   private JPanel screen,buttons;//,helpScreen;
+   private JPanel screen,buttons;
    private JButton play,help;
    private JFrame about;
-   
+      
 	public MenuGui(){
    
-  //  JPanel screen = new JPanel();
-//    LayoutManager overlay = new OverlayLayout(screen);
-//    screen.setLayout(overlay);
-//    
-//    JPanel bg = new JPanel(new FlowLayout());
-//    background = new JLabel(new ImageIcon("battleship_background.png"));//LOOKIT IT'S PRETTY
-//    
-//    bg.add(background, BorderLayout.CENTER);
-//    screen.add(bg);
-//    add(screen, BorderLayout.CENTER);
-//    
-//    buttons = new JPanel();   
-//    play = new JButton("Play Game!");
-//    help = new JButton("How to Play");
-//    
-//    
-//    buttons.add(play);
-//    
-//    buttons.add(help);
-//    
-//    
-//    screen.add(buttons);
-
    JPanel screen = new JPanel();
    screen.setLayout(null);
    screen.setSize(WIDTH,HEIGHT);
    screen.setLocation(0,0);
    
-   background = new JLabel(new ImageIcon("battleship_background.png"));//LOOKIT IT'S PRETTY
-   background.setSize(1000,1000);
-   background.setLocation(0,0);
-   screen.add(background);
-   
-   buttons = new JPanel();
-   buttons.setLocation(250,250);
-   buttons.setSize(500,500);
-   buttons.setLayout(null);
-   
    play = new JButton("Play Game!");
    play.addActionListener(this);
    play.setSize(250,50);
-   play.setLocation(125,200);
+   play.setLocation(375,400);
    
    help = new JButton("How to Play");
    help.addActionListener(this);
    help.setSize(250,50);
-   help.setLocation(125,280);
+   help.setLocation(375,500);
    
-   buttons.add(play);
-   buttons.add(help);
-   
-   screen.add(buttons, BorderLayout.CENTER);
+   background = new JLabel(new ImageIcon("battleship_background.png"));//LOOKIT IT'S PRETTY
+   background.setSize(1000,1000);
+   background.setLocation(0,0);
+   screen.add(background);
+   //buttons.add(play);
+   //buttons.add(help);
+   screen.add(play);
+   screen.add(help);
+   //screen.add(buttons, BorderLayout.CENTER);
+   screen.revalidate();
+   screen.repaint();
    add(screen);
   
       
@@ -81,6 +55,8 @@ public class MenuGui	extends JFrame implements ActionListener{
    setResizable(false);
    setVisible(true);
    setDefaultCloseOperation(EXIT_ON_CLOSE);
+   validate();
+   //this.repaint();
    
    helpScreen = new JFrame();
    helpScreen.setSize(WIDTH, HEIGHT);
